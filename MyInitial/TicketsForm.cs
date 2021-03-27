ffe873f293a2ebe834239ae734e54ff4c7f7f085
Rikki Mount <rikkimount@student.tafesa.edu.au>
2021-03-27 18:24:17 +1030

    commit the back stall ticket changes

--- MyInitial/TicketsForm.cs
+++ MyInitial/TicketsForm.cs
@@ -39,7 +39,10 @@ namespace Ticketing
                 { mSection = 2; }
             if (radBox.Checked)
                 { mSection = 3; }
-
+            if (radStall.Checked)
+            {
+                mSection = 4;
+            }
             mTicketPrice = new TicketPrice(mSection, mQuantity, mDiscount);
 
             mTicketPrice.calculatePrice();
